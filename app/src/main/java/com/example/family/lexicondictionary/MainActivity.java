d92e00e2cd40ba7882ab994d199891dac6763cec
Yv Heng <kongyh-wa15@student.tarc.edu.my>
2018-10-15 15:26:11 +0800

    implemented spinner

--- app/src/main/java/com/example/family/lexicondictionary/MainActivity.java
+++ app/src/main/java/com/example/family/lexicondictionary/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.example.family.lexicondictionary.Adapter.HistoryListAdapter;
 
@@ -12,10 +14,12 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     //Add data in the array from database, can be done in onCreate with methods
-    String[] originalWordArray = {""};
-    String[] translatedWordArray = {""};
+    String[] originalWordArray = {"Hello","Yv Heng"};
+    String[] translatedWordArray = {"Hi","Yih Hern"};
+    String[] languages = {"English", "Malay", "Mandarin"};
 
     ListView listView;
+    Spinner translateFromList, translateToList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
 
         HistoryListAdapter historyListAdapter = new HistoryListAdapter(this,
                 originalWordArray, translatedWordArray);
+
+        translateFromList = (Spinner)findViewById(R.id.translateFrom);
+        translateToList = (Spinner)findViewById(R.id.translateTo) ;
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, languages);
+        translateFromList.setAdapter(spinnerAdapter);
+        translateToList.setAdapter(spinnerAdapter);
 
         listView = (ListView) findViewById(R.id.historyList);
         listView.setAdapter(historyListAdapter);

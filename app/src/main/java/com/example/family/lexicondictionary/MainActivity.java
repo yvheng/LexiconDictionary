ef436d92c008dd887745d03323cefa09efd6ac3b
Yv Heng <kongyh-wa15@student.tarc.edu.my>
2018-10-22 22:56:02 +0800

    activity update

--- app/src/main/java/com/example/family/lexicondictionary/MainActivity.java
+++ app/src/main/java/com/example/family/lexicondictionary/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.family.lexicondictionary;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.family.lexicondictionary.Adapter.HistoryListAdapter;
 
@@ -21,9 +25,13 @@ public class MainActivity extends AppCompatActivity {
     String[] translatedWordArray;
     boolean[] favoriteWord;
 
-    ListView listView;
-    Spinner translateFromList, translateToList;
+    TextView textViewTranslatedWord;
 
+    ListView listView;
+    Spinner translateFromList= (Spinner)findViewById(R.id.translateFrom),
+            translateToList= (Spinner)findViewById(R.id.translateTo);
+    ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this,
+            android.R.layout.simple_spinner_dropdown_item, languages);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,12 +40,10 @@ public class MainActivity extends AppCompatActivity {
         HistoryListAdapter historyListAdapter = new HistoryListAdapter(this,
                 originalWordArray, translatedWordArray, favoriteWord);
 
-        translateFromList = (Spinner)findViewById(R.id.translateFrom);
-        translateToList = (Spinner)findViewById(R.id.translateTo) ;
-        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_dropdown_item, languages);
         translateFromList.setAdapter(spinnerAdapter);
         translateToList.setAdapter(spinnerAdapter);
+        translateFromList.setOnTouchListener(spinnerOnTouch);
+        translateToList.setOnTouchListener(spinnerOnTouch);
 
         listView = (ListView) findViewById(R.id.historyList);
         listView.setAdapter(historyListAdapter);
@@ -45,10 +51,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position,
                                     long id) {
+                Object o = listView.getItemAtPosition(position);
                 //thing to do when it's clicked
                 //using originalWordArray[position] to get position of item clicked)
 
             }
         });
+    }
+
+    private View.OnTouchListener spinnerOnTouch = new View.OnTouchListener(){
+        public boolean onTouch(View v, MotionEvent event){
+            if(event.getAction() == MotionEvent.ACTION_UP){
+                //translate(); or refresh();
+            }
+            return true;
+        }
+    };
+
+    public void exchange(View v){
+        Spinner temp= (Spinner)findViewById(R.id.translateFrom);
+        temp.setAdapter(spinnerAdapter);
+        temp.setSelection(translateFromList.getSelectedItemPosition());
+        translateFromList.setSelection(translateToList.getSelectedItemPosition());
+        translateToList.setSelection(temp.getSelectedItemPosition());
     }
 }

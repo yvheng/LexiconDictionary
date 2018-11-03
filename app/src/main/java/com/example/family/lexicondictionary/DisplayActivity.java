f484f9ad474939daabb3d52a10dea0080470cc70
Yv Heng <kongyh-wa15@student.tarc.edu.my>
2018-11-03 21:32:58 +0800

    display activity

--- app/src/main/java/com/example/family/lexicondictionary/DisplayActivity.java
+++ app/src/main/java/com/example/family/lexicondictionary/DisplayActivity.java
@@ -1,13 +1,27 @@
 package com.example.family.lexicondictionary;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class DisplayActivity extends AppCompatActivity {
-
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            progressBar.setProgressTintList(ColorStateList.valueOf(Color.GREEN));
+        }else{
+            Drawable progressDrawable = progressBar.getIndeterminateDrawable().mutate();
+            progressDrawable.setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_IN);
+            progressBar.setProgressDrawable(progressDrawable);
+        }
     }
 }
